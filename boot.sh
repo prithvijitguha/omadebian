c948a85267d8b379953bceefe754273c4b450328
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-11 20:49:49 +0530

    updated boot sh install file'

--- boot.sh
+++ boot.sh
@@ -21,16 +21,16 @@ colors=(
   '\033[38;5;51m' # Cornflower Blue
   '\033[38;5;45m' # Royal Blue
 )
+IFS=$'\n' read -rd '' -a lines <<<"$ascii_art" || true
 
+echo ""
+echo ""
 # Split the ASCII art into lines
-IFS=$'\n' read -rd '' -a lines <<<"$ascii_art"
-
 # Print each line with the corresponding color
 for i in "${!lines[@]}"; do
   color_index=$((i % ${#colors[@]}))
-  echo -e "${colors[color_index]}${lines[i]}"
+  echo -e "${colors[color_index]}${lines[i]}\033[0m"
 done
-
 echo "=> Warning: Meradeb is recommeded for Debian 13 Trixie setups"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
