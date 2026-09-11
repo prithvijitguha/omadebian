0efe8134a83b95d07ba0ea1df52a4ce87d07e0b1
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-11 23:04:50 +0530

    Updated gnome set theme into meradeb

--- bin/meradeb-sub/set-gnome-theme.sh
+++ bin/meradeb-sub/set-gnome-theme.sh
@@ -36,4 +36,5 @@ if [ -n "$BACKGROUND" ] && [ "$BACKGROUND" != "<< Back" ]; then
   set_background "$BACKGROUND_DIR/$BACKGROUND"
 fi
 
+source "$MERADEB_PATH/bin/meradeb-sub/set-gnome-theme.sh"
 source "$MERADEB_PATH/bin/meradeb-sub/menu.sh"
