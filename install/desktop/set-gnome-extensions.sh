1db6227b0bb08f69e5e221230973e75b2f72e166
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2026-09-12 11:51:21 +0530

    Updated gnome settings adding gnome dash

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -84,3 +84,8 @@ gsettings set org.gnome.shell.extensions.tophat show-disk false
 gsettings set org.gnome.shell.extensions.tophat show-mem true
 gsettings set org.gnome.shell.extensions.tophat show-fs false
 gsettings set org.gnome.shell.extensions.tophat network-usage-unit bytes
+
+# Configure dash-to-dock
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
